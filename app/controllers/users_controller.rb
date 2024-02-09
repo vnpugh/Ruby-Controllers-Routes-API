8d2b7f56eded25b06f7a43f84404408c915aaabe
vnpugh <vnp19@yahoo.com>
2024-02-08 21:46:35 -0600

    updating files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
-end
+    def index
+      @users = User.all
+      render json: @users
+    end
+  
+    def create
+      @user = User.new(name: params[:name], email: params[:email])
+      if @user.save
+        render json: @user
+      else
+        render json: { error: "Unable to create user." }
+      end
+    end
+  end
